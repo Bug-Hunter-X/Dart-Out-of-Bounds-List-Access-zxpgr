fe61cb860332d82518ce7dee196b56c9ecbcef19
BugHunter-X <askzenai@gmail.com>
2025-01-11 13:14:04 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,8 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+
+// This will throw an error because you're trying to access an index that's out of bounds.
+int lastNumber = numbers[numbers.length];
+
+print(lastNumber);
+```
